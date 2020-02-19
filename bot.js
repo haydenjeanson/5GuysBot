84b00f19be9a5e47ef89347a597e866341a11c22
Hayden <haydenjeanson@gmail.com>
2020-02-18 23:08:22 -0500

    Added support for custom insults

--- bot.js
+++ bot.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
 const getJSON = require('get-json');
+const fs = require('fs');
 
 // Initialize Discord Bot
 const bot = new Discord.Client();
+const INSULT_PATH = 'insults.txt'
 
 bot.on('ready', () => {
     console.log('I am ready!');
@@ -17,32 +19,64 @@ bot.on('message', async (message) => {
 
     if (message.content.substring(0, 1) == '!') {
         var args = message.content.substring(1).split(' ');
+        var cmd = args[0];
 
-        switch(args[0].toLowerCase()) {
-            // !ping
+        args = args.slice(1);
+
+        switch(cmd.toLowerCase()) {
             case 'ping':
                 message.channel.send('Pong!')
             break;
+
             case 'marco':
                 message.channel.send('Polo!');
             break;
+
             case 'insult':
                 var user = ""
-                if (args.length > 1) {
+                if (args.length > 0) {
                     user = message.mentions.members.first();
                 }
 
-                getJSON('https://www.reddit.com/r/insults/top.json?t=month', (error, response) => {
-                    numInsults = response.data.children.length
-                    rndNum = Math.round((Math.random() * numInsults) - 1);
+                getJSON('https://www.reddit.com/r/insults/top.json?t=day', (err, response) => {
+                    var insultArr = [];
+                    fs.readFile(INSULT_PATH, 'utf-8', (err, data) => {
+                        if (err) throw err;
+                        insultArr = data.split('\n');
+                        insultArr.pop();
 
-                    if (user != "") {
-                        message.channel.send(user + ', ' + response.data.children[rndNum].data.title);
-                    } else {
-                        message.channel.send(response.data.children[rndNum].data.title);
-                    }
+                        response.data.children.forEach((child) => {
+                            insultArr.push(child.data.title);
+                        });
+
+                        numInsults = insultArr.length;
+                        rndNum = Math.round((Math.random() * numInsults) - 1);
+
+                        if (user != "") {
+                            message.channel.send(user + ', ' + insultArr[rndNum]);
+                        } else {
+                            message.channel.send(insultArr[rndNum]);
+                        }
+                    });
+
+                    
                 });
             break;
+            case 'addinsult':
+                fs.open(INSULT_PATH, 'a', (err, fd) => {
+                    if (err) throw err;
+
+                    var insultStr = '';
+                    args.forEach((item) => {insultStr = insultStr.concat(' ', item)});
+
+                    fs.write(fd, insultStr.trim() + '\n', (err) => {if (err) throw err});
+
+                    fs.close(fd, (err) => {
+                        if (err) throw err;
+                    });
+                });
+            break;
+
             case 'help':
                 message.channel.send('Available Commands:\n\n!ping - The bot responds Pong!\n!marco - The bot responds Polo!\n!insult - Sends an insult randomly chosen from the top posts of the last month on r/insults')
             break;
