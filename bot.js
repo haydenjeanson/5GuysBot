e4e026ed29fae75bad81ade073de160e5c544c9f
Danny Van Stemp <danwardvs@adsgames.net>
2020-02-24 12:56:56 -0500

    Added a few reddit parsing features, !fact, !joke, !thought, !sub commands

--- bot.js
+++ bot.js
@@ -7,6 +7,67 @@ const fs = require('fs');
 const bot = new Discord.Client();
 const INSULT_PATH = 'insults.txt'
 
+function joke(category,time,message){
+    getJSON('https://www.reddit.com/r/jokes/' + category + '.json?t=' + time, (err, response) => {
+        
+        var titleArr = [];
+        var answerArr = [];
+
+        if(!err){
+            if(response.data.children!=''){
+                response.data.children.forEach((child) => {
+                    titleArr.push(child.data.title);
+                    answerArr.push(child.data.selftext);
+
+                });
+    
+                var num_posts= titleArr.length;
+                var post_load_num = Math.round(Math.random() * (num_posts - 1));
+                message.channel.send(titleArr[post_load_num] + '\n||' + answerArr[post_load_num] + '||');
+            }else{
+                message.channel.send("Empty response.");
+
+            }
+            
+        }else{
+            message.channel.send("Invalid URL.");
+            
+        }
+    }).catch(function(){
+        console.log("Error has occured");
+    });   
+}
+
+function parseSubreddit(subName,category,time,message){
+
+    getJSON('https://www.reddit.com/r/' + subName + '/' + category + '.json?t=' + time, (err, response) => {
+        
+        var subArr = [];
+
+        if(!err){
+            if(response.data.children!=''){
+                response.data.children.forEach((child) => {
+                    subArr.push(child.data.title);
+                });
+    
+                var num_posts= subArr.length;
+                var post_load_num = Math.round(Math.random() * (num_posts - 1));
+                message.channel.send(subArr[post_load_num]);
+            }else{
+                message.channel.send("Empty subreddit.");
+
+            }
+            
+        }else{
+            message.channel.send("Invalid subreddit URL.");
+            
+        }
+    }).catch(function(){
+        console.log("Error has occured");
+    });   
+}
+
+
 bot.on('ready', () => {
     console.log('I am ready!');
 });
@@ -77,8 +138,67 @@ bot.on('message', async (message) => {
                 });
             break;
 
+
+            case 'thought':
+                parseSubreddit("showerthoughts","top","month",message);
+               
+            break;
+
+            case 'fact':
+                parseSubreddit("todayilearned","top","month",message);
+               
+            break;
+
+            case 'joke':
+                switch(args.length){
+                    case 0:
+                        joke("top","month",message);
+                    break;
+                    case 1:
+                        joke(args[0],"month",message);
+                    break;
+                    case 2:
+                        joke(args[0],args[1],message);
+                    break;
+
+                }
+            break;
+
+            case 'sub':
+                switch(args.length){
+                    case 0:
+                        message.channel.send("Require a subreddit to use. Try \' sub showerthoughts top all\'");
+                    break;
+                    case 1:
+                        if(args[0] =="help")
+                            message.channel.send("Usage: !sub <subreddit> <category> <time>\nCan be used by: !sub showerthoughts top all\nTime and category can be omitted.")
+                        else
+                            parseSubreddit(args[0],"top","month",message);
+                    break;
+                    case 2:
+                        parseSubreddit(args[0],args[1],"month",message);
+                    break;
+                    case 3:
+                        parseSubreddit(args[0],args[1],args[2],message);
+                    break;
+                }
+    
+
+            break;
+
+
+
             case 'help':
-                message.channel.send('Available Commands:\n\n!ping - The bot responds Pong!\n!marco - The bot responds Polo!\n!insult - Sends an insult randomly chosen from the top posts of the last month on r/insults')
+                message.channel.send('Available Commands:\n\n\
+            !ping - The bot responds Pong!\n\
+            !marco - The bot responds Polo!\n\
+            !insult - Sends an insult randomly chosen from the top posts of the last month on r/insults\n\
+            !thought - Sends an showerthought randomly chosen from the top posts of the last month on r/showerthoughts\n\
+            !fact - Sends a fact randomly chosen from the top posts of the last month on r/todayilearned\n\
+            !sub - Display a post from any subreddit given a name, and optional category and time. See !sub help\n\
+            !joke - Sends a joke randomly chosen from r/jokes, from top month by default, or any category or time given \n\
+            ')
+                    
             break;
          }
      }
