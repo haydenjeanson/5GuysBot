bf5bd1117eda89ff454fadfa824ee75ace5e837d
Hayden Jeanson <haydenjeanson@gmail.com>
2021-05-17 16:02:05 -0400

    start of slash commands and firestore integration

---
+++ index.js
@@ -0,0 +1,58 @@
+const {Client, Intents} = require('discord.js');
+const auth = require('./auth.json');
+const getJSON = require('get-json');
+const fs = require('fs');
+
+// Set intents of the client
+const clientIntents = new Intents();
+clientIntents.add('GUILD_MESSAGES');
+clientIntents.add('GUILD_MESSAGE_TYPING');
+// Initialize Discord Bot
+const client = new Client({intents: clientIntents});
+
+const commandData = {
+    name: 'echo',
+    description: 'Replies with your input!',
+    options: [{
+      name: 'input',
+      type: 'STRING',
+      description: 'The input which should be echoed back',
+      required: true,
+    }],
+  };
+
+client.once('ready', () => {
+    console.log('I am ready!'); 
+
+    client.guilds.cache.get(auth.guildID).commands.create(commandData);
+});
+
+client.on('message', async (message) => {
+    if (message.content.substring(0, 1) == '!') {
+        var args = message.content.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.slice(1);
+
+        switch(cmd.toLowerCase()) {
+            case 'ping':
+                message.channel.send('Pong!')
+            break;
+         }
+     }
+});
+
+client.on('interaction', interaction => {
+    // If the interaction isn't a slash command, return
+    if (!interaction.isCommand()) return;
+
+    // Check if it is the correct command
+    if (interaction.commandName === 'echo') {
+        // Get the input of the user
+        const input = interaction.options[0].value;
+        // Reply to the command
+        interaction.reply(input);
+    }
+})
+
+client.login(auth.token);
