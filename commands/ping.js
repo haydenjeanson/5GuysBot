681ea5004dc47a1f63458e1b30a9b76b369d9a49
Hayden Jeanson <haydenjeanson@gmail.com>
2021-05-18 15:19:47 -0400

    documentation

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,13 @@
+const { CommandInteraction } = require("discord.js");
+
 module.exports = {
     name: 'ping',
     description: 'Replies with Pong!',
     options: [],
+    /**
+     * Handler for the ping command.
+     * @param {CommandInteraction} interaction 
+     */
     execute(interaction) {
         interaction.reply('Pong!', {ephemeral: true});
     }
