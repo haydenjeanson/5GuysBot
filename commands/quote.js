58c9f0d3bb006099f8fd94e33265895a7e7424dc
Hayden Jeanson <haydenjeanson@gmail.com>
2021-05-18 14:34:22 -0400

    quotes stored as indices to allow random selection later

--- commands/quote.js
+++ commands/quote.js
@@ -1,18 +1,47 @@
 async function addQuote(db, quotedID, quote) {
     try {
         const docRef = db.collection('users').doc(quotedID);
+        const doc = await docRef.get();
+        if (!doc.exists) {
+            // No such document
+            await docRef.set({
+                0: quote,
+                'maxIndex': 0
+            }); 
+        } else {
+            // Retrieve previous maxIndex and increment as we are about to add a new quote.
+            let maxIndex = (await doc.get('maxIndex')) + 1;
 
-        await docRef.set({
-            quote: quote
-        });
-        // Function exectuted successfully
-        return true
+            await docRef.set({
+                [maxIndex]: quote,
+                'maxIndex': maxIndex
+            }, { merge: true }); // Merge with the current doc if it already exists instead of overwriting
+        }
+    
+        // Function executed successfully
+        return true;
     } catch (error) {
         console.error(error);
         // Something went wrong
-        return false
+        return false;
     }    
 }
+
+// async function readQuote(db, quotedID, key) {
+//     try {
+//         const snapshot = await db.collection('users').get();
+//         snapshot.forEach((doc) => {
+//             console.log(doc.id, '=>', doc.data());
+//         });
+
+//         // Function exectuted successfully
+//         return true
+//     } catch (error) {
+//         console.error(error);
+//         // Something went wrong
+//         return false
+//     }    
+// }
 
 module.exports = {
     name: 'quote',
@@ -41,11 +70,12 @@ module.exports = {
         interaction.defer();
 
         // Do the stuff here
-        quoteAdded = addQuote(db, interaction.options[0].options[0].value, interaction.options[0].options[1].value);
+        quoteAdded = await addQuote(db, interaction.options[0].options[0].value, interaction.options[0].options[1].value);
 
         // Edit the deffered response
         if (quoteAdded) {
             interaction.editReply("Quote Added!");
+            // followUp with quote that was added
         } else {
             interaction.editReply("Uh Oh! Something went wrong. The quote was not added.");
         }
